b477269dfeedbdaf427a91798d5c09067f1fba39
Nicolas Spalinger <nicolas.spalinger@sil.org>
2023-02-22 16:43:28 -0600

    Tweaked the backup script a little bit.

--- backup-wsl-repositories.bat
+++ backup-wsl-repositories.bat
@@ -4,15 +4,30 @@
 
 TITLE WSL repositories backup 
 
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set DateTime=%%a
+
+set Yr=%DateTime:~0,4%
+set Mon=%DateTime:~4,2%
+set Day=%DateTime:~6,2%
+set Hr=%DateTime:~8,2%
+set Min=%DateTime:~10,2%
+set Sec=%DateTime:~12,2%
+
 set BACKUPDIR="%USERPROFILE%\WSL-repositories-backup"
 set WSL-UBUNTU-USER=username
+set LOGFILE=%USERPROFILE%\WSL-repositories-backup-log-%Mon%-%Day%-%Yr%-%Hr%-%Min%.txt
 
 echo Creating a backup of repositories in WSL to %BACKUPDIR%  (repos\wstechfonts)
-echo Assuming your Ubuntu VM in WSL is mapped as a network drive to W:
+echo Assuming your Ubuntu VM in WSL is mapped as the W: network drive and your WSL-UBUNTU-USER is properly set
 
+robocopy W:\home\%WSL-UBUNTU-USER%\repos\wstechfonts  %BACKUPDIR%  /MIR /FFT /Z /ETA /LOG:%LOGFILE%
 
-robocopy W:\home\%WSL-UBUNTU-USER%\repos\wstechfonts  %BACKUPDIR%  /MIR /FFT /NFL /Z /ETA 
+echo:
+echo:
 
-echo Done
+echo Done, press any key to close
 
-PAUSE 
+echo:
+echo:
+
+timeout /t 20
